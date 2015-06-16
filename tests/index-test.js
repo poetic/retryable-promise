50ea49776f965907fa6cb39f3f04b1e63f0a9885
osxi <osxi@noreply.github.com>
2015-06-16 10:18:00 -0500

    Add timeout option when retrying

--- tests/index-test.js
+++ tests/index-test.js
@@ -41,15 +41,36 @@ describe('retryablePromise', function() {
 
     it('promise fails all 5 times', function() {
       return retryablePromise(failingPromise(6), options).catch(function(){
-        assert.ok(true, 'Promise failed three times');
+        assert.ok(true, 'Promise failed five times');
       });
     });
 
     it('promise only fails 4 times', function() {
       return retryablePromise(failingPromise(4), options).then(function(){
         assert.ok(true, 'Promise resolved successfully');
-      }, function() {
+      });
+    });
+  });
 
+  describe('timeout option', function() {
+    it('defaults to 0 (immediate)', function() {
+      var start = new Date();
+      return retryablePromise(failingPromise(2)).then(function() {
+        var end  = new Date();
+        var diff = start.getTime() - end.getTime() < 5;
+        assert.ok(true, 'Promise waits 1.5 second to retry');
+      });
+    });
+
+    it('waits for 1.5 seconds', function() {
+      this.timeout(2000);
+      var start = new Date();
+      return retryablePromise(failingPromise(2), {
+        timeout: 1500
+      }).then(function() {
+        var end  = new Date();
+        var diff = start.getTime() - end.getTime() > 1000;
+        assert.ok(true, 'Promise waits 1.5 second to retry');
       });
     });
   });
